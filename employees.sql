1c1c1d75fe51f39c61021604b1148cf84fe37343
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-10 19:06:44 +0100

    do not load any data, just create database

--- employees.sql
+++ employees.sql
@@ -121,19 +121,21 @@ CREATE OR REPLACE VIEW current_dept_emp AS
 
 flush /*!50503 binary */ logs;
 
-SELECT 'LOADING departments' as 'INFO';
-source load_departments.dump ;
-SELECT 'LOADING employees' as 'INFO';
-source load_employees.dump ;
-SELECT 'LOADING dept_emp' as 'INFO';
-source load_dept_emp.dump ;
-SELECT 'LOADING dept_manager' as 'INFO';
-source load_dept_manager.dump ;
-SELECT 'LOADING titles' as 'INFO';
-source load_titles.dump ;
-SELECT 'LOADING salaries' as 'INFO';
-source load_salaries1.dump ;
-source load_salaries2.dump ;
-source load_salaries3.dump ;
+
+SELECT 'NOT LOADING ANY DATA' as 'INFO';
+-- SELECT 'LOADING departments' as 'INFO';
+-- source load_departments.dump ;
+-- SELECT 'LOADING employees' as 'INFO';
+-- source load_employees.dump ;
+-- SELECT 'LOADING dept_emp' as 'INFO';
+-- source load_dept_emp.dump ;
+-- SELECT 'LOADING dept_manager' as 'INFO';
+-- source load_dept_manager.dump ;
+-- SELECT 'LOADING titles' as 'INFO';
+-- source load_titles.dump ;
+-- SELECT 'LOADING salaries' as 'INFO';
+-- source load_salaries1.dump ;
+-- source load_salaries2.dump ;
+-- source load_salaries3.dump ;
 
 source show_elapsed.sql ;
