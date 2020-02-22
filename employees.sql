97f85a7b8ae476a8a73b455cc51179be63484011
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-02-22 09:18:09 +0100

    make sure all to_dates have end of all times as default, load salary groups

--- employees.sql
+++ employees.sql
@@ -73,7 +73,7 @@ CREATE TABLE dept_manager (
    -- dept_no      CHAR(4)         NOT NULL,
    dept_no      CHAR(4)         NOT NULL REFERENCES departments(dept_no),
    from_date    DATE            NOT NULL,
-   to_date      DATE            NOT NULL,
+   to_date      DATE            NOT NULL DEFAULT '9999-12-31',
    FOREIGN KEY (emp_no)  REFERENCES employees (emp_no)    ON DELETE CASCADE,
    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
    PRIMARY KEY (emp_no,dept_no, from_date)
@@ -85,7 +85,7 @@ CREATE TABLE dept_emp (
     -- dept_no     CHAR(4)         NOT NULL,
     dept_no     CHAR(4)         NOT NULL REFERENCES departments(dept_no),
     from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
+    to_date     DATE            NOT NULL DEFAULT '9999-12-31',
     FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no)  ON DELETE CASCADE,
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,dept_no, from_date)
@@ -96,7 +96,7 @@ CREATE TABLE titles (
     emp_no      INT             NOT NULL REFERENCES employees(emp_no),
     title       VARCHAR(50)     NOT NULL,
     from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
+    to_date     DATE            NOT NULL DEFAULT '9999-12-31',
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,title, from_date)
 )
@@ -107,7 +107,7 @@ CREATE TABLE salaries (
     emp_no      INT             NOT NULL REFERENCES employees(emp_no),
     salary      INT             NOT NULL,
     from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
+    to_date     DATE            NOT NULL DEFAULT '9999-12-31',
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no, from_date)
 )
@@ -118,7 +118,7 @@ CREATE TABLE salary_groups (
     sg_name     VARCHAR(40)     NOT NULL COMMENT 'name of the salary group, e.g. EG 12',
     base_salary FLOAT           NOT NULL COMMENT 'monthly base salary for 35h week',
     from_date   DATE            NOT NULL COMMENT 'begin of validity',
-    to_date     DATE            NOT NULL COMMENT 'end of validity',
+    to_date     DATE            NOT NULL DEFAULT '9999-12-31' COMMENT 'end of validity',
     PRIMARY     KEY (sg_no, from_date)
 )
 ;
@@ -129,7 +129,7 @@ CREATE TABLE sg_emp (
     -- sg_no       INT             NOT NULL,
     sg_no       INT             NOT NULL REFERENCES salary_groups(sg_no),
     from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
+    to_date     DATE            NOT NULL DEFAULT '9999-12-31',
     FOREIGN KEY (emp_no)  REFERENCES employees (emp_no)  ON DELETE CASCADE,
     FOREIGN KEY (sg_no)   REFERENCES salary_groups (sg_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,sg_no, from_date)
@@ -167,5 +167,7 @@ SELECT ' * LOADING salaries' as 'INFO';
 source load_salaries1.dump ;
 source load_salaries2.dump ;
 source load_salaries3.dump ;
+SELECT ' * LOADING salary groups' as 'INFO';
+source load_salary_groups.dump ;
 
 source show_elapsed.sql ;
